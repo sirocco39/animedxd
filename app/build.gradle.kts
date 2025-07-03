38279893e36d4ba166565fec48ec3ac63f706c29
DvNAlvN <davialviano79@gmail.com>
2025-07-03 16:19:58 +0700

    Master layout.Perbaikan push, tadi salah push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("androidx.fragment:fragment:1.6.2")
+    implementation("com.google.android.material:material:1.11.0")
+
+
 }
